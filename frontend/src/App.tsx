8ea25c37f2db4e650da0ca00921d66823d5198bd
hfxz <xuan__zai@outlook.com>
2025-04-09 21:24:20 +0800

    finish simple frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,85 @@
 import { useState } from 'react'
+import { Loader2 } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+
 import './App.css'
-import { Button } from "@/components/ui/Button"
+
+enum Status { idle, submitting, success, error }
+
+function SubmitButton(props: {status: Status}) {
+  let status = props.status
+  if (status !== Status.submitting) return (
+    <Button> Get Score </Button>
+  )
+  else return (
+    <Button disabled>
+      <Loader2 className="animate-spin" />
+      Please wait
+    </Button>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [review, setReview] = useState<string>('')
+  const [score, setScore] = useState<number>(0)
+  const [error, setError] = useState<string>('No further infomation')
+  const [status, setStatus] = useState<Status>(Status.idle)
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!review.trim()) return; // 空内容不提交
+
+    setStatus(Status.submitting);
+
+    fetch('/api/score', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ review }),
+    }).then((response) => {
+      if (!response.ok) {
+        throw new Error(`Request failed with code ${response.status}`);
+      }
+      return response.json()
+    }).then((data) => {
+      setScore(data.score)
+      setStatus(Status.success)
+    }).catch((err) => {
+      setError(err instanceof Error ? err.message : 'Unknown error');
+      setStatus(Status.error);
+    })
+  }
 
   return (
     <>
-      <Button> Click me </Button>
+      <form
+        onSubmit={handleSubmit}
+        className="flex w-full max-w-sm items-center space-x-2"
+      >
+        {/* TODO: turn to textarea  */}
+        <Input
+          type="text"
+          value={review}
+          onChange={(e) => setReview(e.target.value)}
+          placeholder="Input your review here"
+          disabled={status === Status.submitting}
+        />
+        <SubmitButton status={status} />
+      </form>
+
+      { status === Status.success && (
+        <p className="text-base font-mono">Your score is: {score}</p>
+      )}
+
+      { status === Status.error && (
+        <div className="text-base font-mono">
+          <p className="text-red-400 font-bold">Oops, something went wrong.</p>
+          {error}
+        </div>
+      )}
     </>
   )
 }
